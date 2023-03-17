c5371ab522d0f70199c189209efb67494982b3dc
misha_fox <usmishafox13@gmail.com>
2023-03-17 17:37:11 +0400

    hw15

---
+++ HW15/conftest.py
@@ -0,0 +1,41 @@
+import pytest
+from hw15.functional.human import Human
+
+
+@pytest.fixture()
+def create_dude():
+    yield Human('Endy', 27, 'male')
+    # print('\n some kind tear down')
+
+
+@pytest.fixture()
+def create_female(create_dude):
+    dude = create_dude
+    dude.change_gender('female')
+    yield dude
+
+
+@pytest.fixture()
+def create_old_dude():
+    yield Human('Peter', 99, 'male')
+
+
+@pytest.fixture()
+def create_dead(create_old_dude):
+    dude = create_old_dude
+    dude.grow()
+    dude.grow()
+    yield dude
+
+
+@pytest.fixture()
+def create_human_with_params():
+    def __create_human(name: str, age: int, gender: str):
+        return Human(name, age, gender)
+
+    yield __create_human
+
+
+@pytest.fixture
+def create_wrong_gender():
+    yield Human('Kelly', 17, 'new gender')
